70980f7bd4bb86374cef6ed0e4986331ba4bcb23
hl49 <henrylema94@gmail.com>
2022-03-25 13:42:00 -0700

    pixhawk imu added

--- scripts/gimbal_controller_node.py
+++ scripts/gimbal_controller_node.py
@@ -2,8 +2,9 @@
 
 import rospy
 import numpy as np
+import rostopic
 from sensor_msgs.msg import JointState
-
+from std_msgs.msg import Float32MultiArray
 
 class Gimbal:
     # def shutdown():
@@ -14,7 +15,9 @@ class Gimbal:
         # Node frequency
         self.frequency = 1.0/10.0 
         self.taget = []
-        self
+        self.servo_data_seq_counter = 0
+        self.tilt = 0.0
+        self.roll = 0.0
 
         # Init ROS node
         rospy.init_node("gimbal_controller")
@@ -26,40 +29,51 @@ class Gimbal:
         #         Imu,
         #         self._get_target_callback,
         #         queue_size=1)
-    
-        # self.sub_imu = rospy.Subscriber("/Imu_data",
-        #         Imu,
-        #         self._get_imu_callback,
-        #         queue_size=1)
-
 
         self.servo_pub = rospy.Publisher("/desired_joint_states",
                     JointState,
                     queue_size=1)
 
+        self.sub_imu = rospy.Subscriber("/pixhawk_rpy",
+                Float32MultiArray,
+                self._get_imu_callback,
+                queue_size=1)
+
+
     def get_target_callback(self):
         pass
 
-    def get_imu_callback(self):
-        pass
+    def _get_imu_callback(self, data):
+        # print(data.data)
+        self.tilt = round(data.data[0],2) * -1.0
+        self.roll = round(data.data[1],2) * -1.0
 
     def publish_servos_ref(self):
-        pass
+
+        servo_data = JointState()
+
+        servo_data.header.stamp = rospy.Time.now()
+        servo_data.header.frame_id = ''
+        servo_data.header.seq = self.servo_data_seq_counter
+
+        servo_data.name = ["tilt", "roll"]
+        servo_data.position = [self.tilt, self.roll]
+
+        self.servo_data_seq_counter =+ 1
+        self.servo_pub.publish(servo_data)
+
 
     def run(self, event=None):
-
-        print("hey!")
-        # print(type(event))
-
-        # calc pid
 
         # if there is not target position, set 0. 
 
         # set servos
-
+        self.publish_servos_ref()
+        
 
 
 if __name__ == "__main__":
+
 
     gimbal = Gimbal()
 
@@ -67,9 +81,4 @@ if __name__ == "__main__":
 
     rospy.spin()
 
-    # try:
-    #     gimbal.run()
-
-    # except rospy.ROSInterruptException:
-    #     pass
-    #     # shutdown_controller()
+    
