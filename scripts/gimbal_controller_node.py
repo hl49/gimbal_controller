1e0ad1bac048ff3fa41bb76584871d8f25fee9c6
hl49 <henrylema94@gmail.com>
2022-04-08 14:14:56 -0700

    motion-fit.py get init position

--- scripts/gimbal_controller_node.py
+++ scripts/gimbal_controller_node.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# from atexit import unregister
+from logging import shutdown
+from ntpath import join
 import rospy
 import numpy as np
 import math
@@ -18,6 +21,13 @@ class Gimbal:
         self.servo_data_seq_counter = 0
         self.tilt = 0.0
         self.roll = 0.0
+        self.ini_tilt = 0.0
+        self.ini_roll = 0.0
+        self.count_init_servo_pos = 0
+        self.num_samples_init = 20
+        self.ini_tilt_arr = np.zeros([self.num_samples_init, 1])
+        self.ini_roll_arr = np.zeros([self.num_samples_init, 1])
+
         # self.max_tilt = math.radians(65)
         # self.max_roll = math.radians(45)
 
@@ -34,18 +44,32 @@ class Gimbal:
 
         self.servo_pub = rospy.Publisher("/desired_joint_states",
                     JointState,
-                    queue_size=1)
+                    queue_size = 1)
+
+        self.sub_joint_states = rospy.Subscriber("/joint_states",
+                    JointState,
+                    self.get_ini_joint_states_callback,
+                    queue_size = 1)
 
         self.sub_imu = rospy.Subscriber("/pixhawk_rpy",
-                Float32MultiArray,
-                self._get_imu_callback,
-                queue_size=1)
+                    Float32MultiArray,
+                    self.get_imu_callback,
+                    queue_size=1)
 
 
-    def get_target_callback(self):
-        pass
+    def get_ini_joint_states_callback(self, data):
+        if self.count_init_servo_pos < self.num_samples_init:
+            joints = dict(zip(data.name, data.position))
+            self.ini_tilt_arr[self.count_init_servo_pos, 0] = joints['tilt']
+            self.ini_roll_arr[self.count_init_servo_pos, 0] = joints['roll']
+            self.count_init_servo_pos += 1
+        else:
+            self.ini_tilt = self.ini_tilt_arr.mean()
+            self.ini_roll = self.ini_roll_arr.mean()
+            self.sub_joint_states.unregister()
+        
 
-    def _get_imu_callback(self, data):
+    def get_imu_callback(self, data):
         # print(data.data)
         self.tilt = round(data.data[0],2) * -1.0
         self.roll = round(data.data[1],2) * -1.0
@@ -67,7 +91,7 @@ class Gimbal:
         self.servo_pub.publish(servo_data)
 
 
-    def run(self, event=None):
+    def stabilize(self, event=None):
 
         # if there is not target position, set 0. 
 
@@ -82,7 +106,7 @@ if __name__ == "__main__":
 
     gimbal = Gimbal()
 
-    rospy.Timer(rospy.Duration(gimbal.frequency), gimbal.run)
+    rospy.Timer(rospy.Duration(gimbal.frequency), gimbal.stabilize)
 
     rospy.spin()
 
