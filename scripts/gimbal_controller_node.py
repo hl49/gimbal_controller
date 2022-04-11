353139b6cf40881e100ff156265e40edb20a88d6
hl49 <henrylema94@gmail.com>
2022-04-11 15:20:52 -0700

    open loop

--- scripts/gimbal_controller_node.py
+++ scripts/gimbal_controller_node.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 # from atexit import unregister
-from logging import shutdown
-from ntpath import join
+# from logging import setLogRecordFactory, shutdown
+# from ntpath import join
 import rospy
 import numpy as np
 import math
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float32MultiArray
+from perception.msg import RPYAxes 
 
 class Gimbal:
 
@@ -15,16 +16,20 @@ class Gimbal:
 
         # Set variables
         # Node frequency
-        self.frequency = 10.0
+        self.frequency = 10.0 # Hz
         self.pos_initilized = False
         self.servo_data_seq_counter = 0
         self.tilt = 0.0
         self.roll = 0.0
-        self.ref_pos = {'tilt':0.0, 'roll': 0.0}
-        self.target_pos = {'tilt':0.0, 'roll': 0.0}
-        self.ini_pos = {'tilt':0.0, 'roll':0.0}
+        self.ref_pos = {'tilt':0.0, 'roll': 0.0} # From perception: input
+        self.target_pos = {'tilt':0.0, 'roll': 0.0} # Command we send to servos: output
+        self.ini_pos = {'tilt':0.0, 'roll':0.0} # 
+        self.tilt_limit_max = math.radians(45)
+        self.tilt_limit_min = math.radians(-45)
+        self.roll_limit_max = math.radians(45)
+        self.roll_limit_min = math.radians(-45)
         self.count_init_servo_pos = 0 
-        self.num_samples_init = 20
+        self.num_samples_init = 20 # Number of samples (ms)
         self.ini_tilt_arr = np.zeros([self.num_samples_init, 1])
         self.ini_roll_arr = np.zeros([self.num_samples_init, 1])
 
@@ -51,10 +56,10 @@ class Gimbal:
                     self.get_ini_joint_states_callback,
                     queue_size = 1)
 
-        self.sub_imu = rospy.Subscriber("/pixhawk_rpy",
-                    Float32MultiArray,
-                    self.get_imu_callback,
-                    queue_size=1)
+        self.sub_imu = rospy.Subscriber("/perception_rpy",
+                    RPYAxes,
+                    self.get_perception_data_callback,
+                    queue_size = 1)
 
 
     def get_ini_joint_states_callback(self, data):
@@ -64,20 +69,23 @@ class Gimbal:
             self.ini_roll_arr[self.count_init_servo_pos, 0] = joints['roll']
             self.count_init_servo_pos += 1
         else:
+            # Check variance
             self.ini_pos['tilt'] = self.ini_tilt_arr.mean()
             self.ini_pos['roll'] = self.ini_roll_arr.mean()
             self.pos_initilized = True
             self.sub_joint_states.unregister()
     
 
-    def get_imu_callback(self, data):
+    def get_perception_data_callback(self, data):
         # print(data.data)
-        self.tilt = round(data.data[0],2) * -1.0
-        self.roll = round(data.data[1],2) * -1.0
+        # self.tilt = round(data.data[0],2) * -1.0
+        # self.roll = round(data.data[1],2) * -1.0
+
+        pass
 
         # if self.tilt > - self.max_tilt and self.tilt <
 
-    def pub_servos_ref(self):
+    def pub_servos_pos(self):
 
         servo_data = JointState()
 
@@ -92,21 +100,36 @@ class Gimbal:
         self.servo_pub.publish(servo_data)
 
 
-    def stabilize(self, event=None):
+    def stabilize(self):
 
         # if there is not target position, set 0. 
 
         # Controller
         
         # Frequency of execution
-
+        # Verify with Ariel perception frequency
         rate = rospy.Rate(self.frequency)
 
         while not rospy.is_shutdown():
             if self.pos_initilized == True:
                 self.target_pos['tilt'] = self.ref_pos['tilt'] + self.ini_pos['tilt']
                 self.target_pos['roll'] = self.ref_pos['roll'] + self.ini_pos['roll']
-                self.pub_servos_ref()
+
+
+                # Check position limits in tilt axis
+                if self.target_pos['tilt'] > self.tilt_limit_max:
+                    self.target_pos['tilt'] = self.tilt_limit_max
+                elif self.target_pos['tilt'] < self.tilt_limit_min:
+                    self.target_pos['tilt'] = self.tilt_limit_min
+
+                # Check position limits in roll axis
+                if self.target_pos['roll'] > self.roll_limit_max:
+                    self.target_pos['roll'] = self.roll_limit_max
+                elif self.target_pos['roll'] < self.roll_limit_min:
+                    self.target_pos['roll'] = self.roll_limit_min
+
+                self.pub_servos_pos()
+
             rate.sleep()
 
 
